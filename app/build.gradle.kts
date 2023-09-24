b32ae4ba37d321d7f730c8c603bdd57fd704666e
abdul rohman <abdulrohmanunida@gmail.com>
2023-09-24 09:37:47 +0700

    call action with NavController at HomeFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.best.mynavigation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.best.mynavigation"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
